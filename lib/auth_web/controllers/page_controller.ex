2881b5f44566a4e7c1fcb93abd2d83db3577eea9
beppo <beppo@localhost.localdomain>
2021-01-24 13:12:42 -0600

    require auth in routes

--- lib/auth_web/controllers/page_controller.ex
+++ lib/auth_web/controllers/page_controller.ex
@@ -1,6 +1,10 @@
 defmodule AuthWeb.PageController do
   use AuthWeb, :controller
 
+  import AuthWeb.UserAuth, only: [require_authenticated_user: 2]
+
+  plug :require_authenticated_user when action not in [:index]
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
